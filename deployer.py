b87b0e9bd58e5ac684d2ceda433d5752a2437913
Meraj Kashi <mkashi@eikplatform.io>
2021-04-19 09:02:39 +0200

    Initiate deployer script

--- deployer.py
+++ deployer.py
@@ -3,13 +3,55 @@ import re
 import time
 import os
 
-# Deploy 3 nodes with MLN installed Docker on top
+rebuild_list = []
+error = 1
 
-# Initiate Swarm cluster
 
-get_master_ip=os.popen("nova list | grep swarm-1 ")
-read_master_ip=get_master_ip.read()
-master_ip=re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",read_master_ip)
+def handle_error():
+    global error
+    global rebuild_list
+    while error:
+        failed = os.popen('nova list | grep "ERROR"')
+        error_list = failed.read()
+        error_array = re.findall(" [a-zA-Z0-9]*.projectx", error_list)
 
-print(master_ip)
+        if error_array:
+            for i in error_array:
+                rebuild_list.append((i.replace(" ", "")).replace(".projectx", ""))
+        else:
+            error = 0
+            rebuild_list = []
+
+        for i in rebuild_list:
+            subprocess.run('nova delete ' + i + '.projectx', shell=True)
+            time.sleep(10)
+            subprocess.run('mln start -p projectx -h ' + i, shell=True)
+            time.sleep(20)
+        rebuild_list = []
+        
+
+
+def vm_deploy():
+
+    print('------------- Start Deployment ------------')
+    # Build mln project
+    subprocess.run('mln build -f projectx.mln -r', shell=True)
+    time.sleep(30)
+
+    # Start deployment infra with MLN
+    subprocess.run('mln start -p projectx', shell=True)
+    time.sleep(30)
+
+    handle_error()
+
+def swarm_deploy():
+    print('------------- Start Swarm Deployment ------------')
+    subprocess.run('chmode +x ./swarm.sh', shell=True)
+    subprocess.run('./swarm.sh', shell=True)
+
+
+
+vm_deploy()
+swarm_deploy()
+
 
